86a96cf342dcd613ac1a0db60cd36093282157ba
qian wang <1510106069@qq.com>
2024-01-12 10:19:00 +0800

    feat: useUpdate

---
+++ src/hooks/useUpdate/index.ts
@@ -0,0 +1,9 @@
+import {useCallback, useState} from "react";
+
+const useUpdate = () => {
+  const [, setState] = useState({});
+  // 返回一个函数，通过变更 state，使组件重新渲染
+  return useCallback(() => setState({}), []);
+};
+
+export default useUpdate;
