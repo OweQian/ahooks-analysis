88f3fb627a20117e73e5c9c538c2994bbb3394a7
qian wang <1510106069@qq.com>
2024-03-23 23:27:00 +0800

    feat: useUnmount单测

--- src/hooks/useUnmount/index.ts
+++ src/hooks/useUnmount/index.ts
@@ -1,16 +1,19 @@
 import { isFunction } from "../../../utils";
+import isDev from "../../../utils/isDev";
 import { useEffect } from "react";
 import useLatest from "@/hooks/useLatest";
 
 const useUnmount = (fn: () => void) => {
-  if (!isFunction(fn)) {
-    console.error(
-      `useUnmount expected parameter is a function, but got ${typeof fn}`
-    );
+  if (isDev) {
+    if (!isFunction(fn)) {
+      console.error(
+        `useUnmount expected parameter is a function, but got ${typeof fn}`
+      );
+    }
   }
 
   const fnRef = useLatest(fn);
-  // useEffect 的返回值中执行函数
+  // 组件卸载执行函数
   useEffect(() => () => fnRef.current?.(), []);
 };
 
