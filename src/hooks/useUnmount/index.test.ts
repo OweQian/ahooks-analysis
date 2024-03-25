33b6854f63d5cb2929b16ad6befce706f2b423ff
qian wang <1510106069@qq.com>
2024-03-25 16:58:46 +0800

    feat: 单测

--- src/hooks/useUnmount/index.test.ts
+++ src/hooks/useUnmount/index.test.ts
@@ -1,25 +1,17 @@
 import { renderHook } from "@testing-library/react";
 import useUnmount from "./index";
 
-// 定义测试套件
 describe("useUnmount", () => {
-  // 定义测试用例
-  it("useUnmount", async () => {
-    //  创建一个 mock 函数 fn
+  it("useUnmount should work", async () => {
     const fn = jest.fn();
-    // 渲染 useUnmount Hook，并将 fn 作为参数传递给 useUnmount
+
     const hook = renderHook(() => useUnmount(fn));
-    // 断言验证函数 fn 被调用的次数是否为 0
     expect(fn).toHaveBeenCalledTimes(0);
 
-    // 重新渲染
     hook.rerender();
-    // 断言验证函数 fn 被调用的次数是否为 0
     expect(fn).toHaveBeenCalledTimes(0);
 
-    // 卸载
     hook.unmount();
-    // 断言验证函数 fn 被调用的次数是否为 1
     expect(fn).toHaveBeenCalledTimes(1);
   });
 });
--- src/hooks/useUnmountedRef/index.test.ts
+++ src/hooks/useUnmountedRef/index.test.ts
@@ -1,23 +1,15 @@
 import { renderHook } from "@testing-library/react";
 import useUnmountedRef from "./index";
 
-// 定义测试套件
 describe("useUnmountedRef", () => {
-  // 定义测试用例
   it("useUnmountedRef", async () => {
-    // 渲染 useUnmountedRef Hook
     const hook = renderHook(() => useUnmountedRef());
-    // 断言组件是否已经卸载
     expect(hook.result.current.current).toBe(false);
 
-    // 重新渲染
     hook.rerender();
-    // 断言组件是否已经卸载
     expect(hook.result.current.current).toBe(false);
 
-    // 卸载
     hook.unmount();
-    // 断言组件是否已经卸载
     expect(hook.result.current.current).toBe(true);
   });
 });
